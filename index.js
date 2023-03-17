f12297ea29893e8e8d3f240e2cd052a87f717db1
titus <titus.unavailable@student.moringaschool.com>
2023-03-17 12:28:42 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+/* multiply */
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply)
+
+/* random */
+Math.floor(Math.random() * 100) + 1;
+
+/* mod */
+const num3 = 9;
+const num4 = 5;
+const mod = num3%num4;
+
+/* max */
+const max = math.max(2,7,17,20);
+
